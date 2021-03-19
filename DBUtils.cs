37218e2b2fee629d9da7d6afb65ed062f6a55577
GoForthSMOrc <79578581+GoForthSMOrc@users.noreply.github.com>
2021-03-19 16:36:44 +0700

    Add files via upload

---
+++ DBUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Pr6
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "localhost";
+            int port = 3306;
+            string database = "KnockOut";
+            string username = "student";
+            string password = "student";
+
+            return DBMySQLUtils.GetConnection(host, port, database, username, password);
+        }
+    }
+}
